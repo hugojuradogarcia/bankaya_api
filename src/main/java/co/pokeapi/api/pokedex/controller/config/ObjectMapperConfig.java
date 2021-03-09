ab479fa8f180762b3b1396f49bab2a2a4e6680a3
Hugo Jurado <hugojuradogarcia@gmail.com>
2021-03-08 19:30:37 -0600

    Create controller layer.
    Why is this change necessary?
    
    -  Map correctly.
    -  Be able to implement the client.
    
    How does it address the issue?
    
    -  Create endpoints.
    -  Configure Mapper, RestTemplate and Security.
    
    What side effects does this change have?
    
    none

---
+++ src/main/java/co/pokeapi/api/pokedex/controller/config/ObjectMapperConfig.java
@@ -0,0 +1,30 @@
+package co.pokeapi.api.pokedex.controller.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class ObjectMapperConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(propertyNamingStrategy());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+
+    @Bean
+    public PropertyNamingStrategy propertyNamingStrategy()
+    {
+        return new PropertyNamingStrategy.SnakeCaseStrategy();
+    }
+
+}
